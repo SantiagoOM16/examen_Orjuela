06587566253b51dbccf886ffbac0682353e275ef
SORJUELA <santiagoorjuela8@gmail.com>
2018-02-27 10:39:03 +0100

    Examen Orjuela

---
+++ examen_Orjuela/src/examen_Orjuela/exercici3.java
@@ -0,0 +1,21 @@
+package examen_Orjuela;
+
+
+public class exercici3
+{
+     public static void main(String[]args)
+
+{
+	  printOne();
+	  printOne();
+	  printTwo();
+}
+     public static void printOne() {
+    	 System.out.println("Hello world");
+     }
+     public static void printTwo() {
+
+   	  printOne();
+   	  printOne();
+     }
+}
